bf4caa961868e89ff2ff8b1763dea5c72076f7e5
Aykhan <78858146+aykhan026@users.noreply.github.com>
2021-04-14 00:47:50 +0400

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -40,7 +40,7 @@ if not client.is_user_authorized():
     client.send_code_request(phone)
     os.system('clear')
     banner()
-    client.sign_in(phone, input(gr+'[+] Enter the code: '+re))
+    client.sign_in(phone, input(gr+'[+] Kodu yazın: '+re))
  
 os.system('clear')
 banner()
